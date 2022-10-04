83c767e99449d72be59c10c15ac244469cade894
rikynurdiana <nurdiana.riky@gmail.com>
2022-10-05 00:26:39 +0700

    chore: tes deploy

--- src/pages/maps.jsx
+++ src/pages/maps.jsx
@@ -1,50 +1,9 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { useNavigate } from "react-router-dom";
-// import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 const Maps = () => {
 	const navigate = useNavigate();
-	// const containerStyle = {
-	// 	width: "400px",
-	// 	height: "400px",
-	// };
-
-	// const center = {
-	// 	lat: -3.745,
-	// 	lng: -38.523,
-	// };
-
-	// const { isLoaded } = useJsApiLoader({
-	// 	id: "google-map-script",
-	// });
-
-	// const [map, setMap] = React.useState(null);
-
-	// const onLoad = React.useCallback(function callback(map) {
-	// 	const bounds = new window.google.maps.LatLngBounds(center);
-	// 	map.fitBounds(bounds);
-	// 	setMap(map);
-	// }, []);
-
-	// const onUnmount = React.useCallback(function callback(map) {
-	// 	setMap(null);
-	// }, []);
-
-	// return isLoaded ? (
-	// 	<GoogleMap
-	// 		mapContainerStyle={containerStyle}
-	// 		center={center}
-	// 		zoom={10}
-	// 		onLoad={onLoad}
-	// 		onUnmount={onUnmount}
-	// 	>
-	// 		{/* Child components, such as markers, info windows, etc. */}
-	// 		<></>
-	// 	</GoogleMap>
-	// ) : (
-	// 	<></>
-	// );
 
 	const handleClickToVideo = () => {
 		console.log("ke video");
