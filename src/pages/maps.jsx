d7184b7b888736b141eddfd52a427245a96868e1
rikynurdiana <nurdiana.riky@gmail.com>
2022-10-06 12:29:38 +0700

    chore: update geolocation map

--- src/pages/maps.jsx
+++ src/pages/maps.jsx
@@ -10,7 +10,7 @@ const Maps = () => {
 	return (
 		<div className="maps-section-content">
 			<iframe
-				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d495.154823676032!2d107.63586435466716!3d-6.861984343027989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e77f69edf551%3A0x14c495539fc36585!2seFishery%20Head%20Office!5e0!3m2!1sid!2sid!4v1664903383350!5m2!1sid!2sid"
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d701.4491451898596!2d106.27050977878842!3d-5.995340941539429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4108c6103da84222!2zNcKwNTknNDMuMSJTIDEwNsKwMTYnMTUuMCJF!5e0!3m2!1sid!2sid!4v1665034054083!5m2!1sid!2sid"
 				width="100%"
 				height="450"
 				allowFullScreen=""
