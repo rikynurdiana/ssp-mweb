db3a1837ef1620dba405aec540ea6f67487aa8da
rikynurdiana <nurdiana.riky@gmail.com>
2022-10-07 13:19:48 +0700

    chore: change box img to png

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 import Modal from "react-bootstrap/Modal";
 
 import PackingImg from "../assets/efish/packaging-bg.png";
-import BoxPacket from "../assets/efish/eFresh-Shrimp-Packaging.svg";
+import BoxPacket from "../assets/efish/eFresh-Shrimp-Packaging.png";
 import ScanBtn from "../assets/efish/scan-button.svg";
 
 const Landing = () => {
